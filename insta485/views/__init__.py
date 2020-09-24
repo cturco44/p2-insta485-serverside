198dbfb04ad6d11376449f625a8528e1890b8df4
eroyama <eroyama@umich.edu>
2020-09-24 19:30:35 -0400

    added modules to __init__.py

--- insta485/views/__init__.py
+++ insta485/views/__init__.py
@@ -4,4 +4,4 @@ from insta485.views.post import show_post
 from insta485.views.post import download_file
 from insta485.views.explore import show_explore
 from insta485.views.edit import show_edit
-from insta485.views.password import show_password
+from insta485.views.password import show_password, hash_password, check_password
